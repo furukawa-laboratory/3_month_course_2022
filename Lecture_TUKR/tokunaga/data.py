c7e8792f78bb2a049e90d1cee3c11c42f8e94ef9
YukiTokunaga <tokunaga.yuki756@gmail.com>
2022-05-18 16:37:26 +0900

    とりあえず実装して動いたよ

--- Lecture_TUKR/tokunaga/data.py
+++ Lecture_TUKR/tokunaga/data.py
@@ -5,15 +5,15 @@ def load_kura_tsom(xsamples, ysamples, missing_rate=None,retz=True):
     z1 = np.linspace(-1, 1, xsamples)
     z2 = np.linspace(-1, 1, ysamples)
 
-    z1_repeated, z2_repeated = np.meshgrid(z1, z2)
-    x1 = z1_repeated.reshape(-1, 1)
-    x2 = z2_repeated.reshape(-1, 1)
-    x3 = x1**2 - x2**2 + + np.random.uniform(-0.5, 0.5, xsamples*ysamples).reshape(-1, 1)
+    z1_repeated, z2_repeated = np.meshgrid(z1, z2, indexing='ij')
+    x1 = z1_repeated
+    x2 = z2_repeated
+    x3 = x1**2 - x2**2 + + np.random.uniform(-0.5, 0.5, xsamples*ysamples).reshape(xsamples, ysamples)
     #ノイズを加えたい時はここをいじる,locがガウス分布の平均、scaleが分散,size何個ノイズを作るか
     #このノイズを加えることによって三次元空間のデータ点は上下に動く
 
-    x = np.concatenate([x1, x2, x3], axis=1)
-    truez = np.concatenate([x1, x2])
+    x = np.concatenate([x1[:, :, None], x2[:, :, None], x3[:, :, None]], axis=2)
+    truez = np.concatenate([x1[:, :, None], x2[:, :, None]], axis=2)
 
     if missing_rate == 0 or missing_rate == None:
         if retz:
@@ -29,9 +29,9 @@ if __name__ == '__main__':
     ysamples = 15
 
     x, truez = load_kura_tsom(xsamples, ysamples)
-    
+    print(x.shape)
     fig = plt.figure(figsize=[5, 5])
     ax_x = fig.add_subplot(projection='3d')
-    ax_x.scatter(x[:, 0], x[:, 1], x[:, 2])
+    ax_x.scatter(x[:, :, 0], x[:, :, 1], x[:, :, 2])
     ax_x.set_title('Generated three-dimensional data')
     plt.show()
--- Lecture_TUKR/tokunaga/tukr.py
+++ Lecture_TUKR/tokunaga/tukr.py
@@ -7,53 +7,68 @@ import jax.numpy as jnp
 import matplotlib.pyplot as plt
 
 class TUKR:
-    def __init__(self, X, latent_dim, sigma, prior='random', Zinit=None):
+    def __init__(self, X, nb_samples1, nb_samples2, latent_dim1, latent_dim2, sigma, prior='random', Uinit=None, Vinit=None):
         #--------初期値を設定する．---------
         self.X = X
         #ここから下は書き換えてね
-        self.nb_samples, self.ob_dim = X.shape
+        self.nb_samples1, self.nb_samples2, self.ob_dim = self.X.shape
         self.sigma = sigma
-        self.latent_dim = latent_dim
+        self.latent_dim1, self.latent_dim2 = latent_dim1, latent_dim2
 
-        if Zinit is None:
+        if Uinit is None:
             if prior == 'random': #一様事前分布のとき
-                Z = np.random.uniform(-0.001, 0.001, self.nb_samples*latent_dim).reshape(self.nb_samples,self.latent_dim)
+                U = np.random.uniform(-0.01, 0.01, self.nb_samples1*self.latent_dim1).reshape(self.nb_samples1, self.latent_dim1)
             else: #ガウス事前分布のとき
-                Z = np.random.normal(0, 0.001, self.nb_samples*latent_dim).reshape(self.nb_samples, self.latent_dim)
+                U = np.random.normal(0, 0.001, self.nb_samples1*self.latent_dim1).reshape(self.nb_samples1, self.latent_dim1)
         else: #Zの初期値が与えられた時
-            Z = Zinit
-        self.Z = Z
+            U = Uinit
+
+        if Vinit is None:
+            if prior == 'random':
+                V = np.random.uniform(-0.001, 0.001, self.nb_samples2 * self.latent_dim2).reshape(self.nb_samples2,
+                                                                                                  self.latent_dim2)
+            else:
+                V = np.random.normal(0, 0.001, self.nb_samples2 * self.latent_dim2).reshape(self.nb_samples2,
+                                                                                            self.latent_dim2)
+        else:
+            V = Vinit
+
+        self.U = U
+        self.V = V
         self.history = {}
 
     def f(self, U, V): #写像の計算
-        su = jnp.sum((U[:, None, :]-U[None, :, :])**2, axis=2)
-        sv = jnp.sum((V[:, None, :]-V[None, :, :])**2, axis=2)
+        su = jnp.sum((U[:, None, :]-self.U[None, :, :])**2, axis=2)
+        sv = jnp.sum((V[:, None, :]-self.V[None, :, :])**2, axis=2)
         ku = self.kernel(su)
         kv = self.kernel(sv)
-        f = jnp.einsum('ni,ni,nd->nd', ku, kv, self.X)/jnp.sum(ku*kv, axis=1, keepdims=True)
+        f = jnp.einsum('il,kj,ijd->ijd', ku, kv, self.X)/jnp.einsum('ii,jj->ij', ku, kv).reshape(nb_samples1, nb_samples2, 1)
         return f
 
-    def kernel(self, s):
-        return np.exp(-1 * s / (2 * self.sigma ** 2))
+    def kernel(self, d):
+        return jnp.exp(-1 * d / (2 * self.sigma ** 2))
 
     def E(self, U, V, X, alpha=0.001, norm=2): #目的関数の計算
-        d = ((X-self.f(U, V))**2)/(self.nb_samples*self.nb_samples)
+        d = ((X-self.f(U, V))**2)/(self.nb_samples1*self.nb_samples2)
         E = jnp.sum(d)+alpha*(jnp.sum(U**norm)+jnp.sum(V**norm))
         return E
 
-    def fit(self, nb_epoch: int, eta: float):
+    def fit(self, nb_epoch: int, ueta: float, veta: float):
         # 学習過程記録用
-        self.history['z'] = np.zeros((nb_epoch, self.nb_samples, self.latent_dim))
-        self.history['f'] = np.zeros((nb_epoch, self.nb_samples, self.ob_dim))
+        self.history['u'] = np.zeros((nb_epoch, self.nb_samples1, self.latent_dim1))
+        self.history['v'] = np.zeros((nb_epoch, self.nb_samples2, self.latent_dim2))
+        self.history['f'] = np.zeros((nb_epoch, self.nb_samples1, self.nb_samples2, self.ob_dim))
         self.history['error'] = np.zeros(nb_epoch)
 
         for epoch in tqdm(range(nb_epoch)):
-            # Zの更新
-            self.Z = self.Z - eta*jax.grad(self.E, argnums=0)(self.Z, self.X)/self.nb_samples
+            # U, Vの更新
+            self.U = self.U - ueta*jax.grad(self.E, argnums=0)(self.U, self.V, self.X)/self.nb_samples1
+            self.V = self.V - veta*jax.grad(self.E, argnums=1)(self.U, self.V, self.X)/self.nb_samples2
             # 学習過程記録用
-            self.history['z'][epoch] = self.Z
-            self.history['f'][epoch] = self.f(self.Z, self.Z)
-            self.history['error'][epoch] = self.E(self.Z, self.X)
+            self.history['u'][epoch] = self.U
+            self.history['v'][epoch] = self.V
+            self.history['f'][epoch] = self.f(self.U, self.V)
+            self.history['error'][epoch] = self.E(self.U, self.V, self.X)
 
     #--------------以下描画用(上の部分が実装できたら実装してね)---------------------
     def calc_approximate_f(self, resolution): #fのメッシュ描画用，resolution:一辺の代表点の数
@@ -93,7 +108,8 @@ if __name__ == '__main__':
     #各種パラメータ変えて遊んでみてね．
     epoch = 200 #学習回数
     sigma = 0.2 #カーネルの幅
-    eta = 50 #学習率
+    ueta = 50 #uの学習率
+    veta = 50 #vの学習率
     latent_dim1 = 2 #潜在空間1の次元
     latent_dim2 = 2 #潜在空間2の次元
 
@@ -101,18 +117,18 @@ if __name__ == '__main__':
     np.random.seed(seed)
 
     #入力データ（詳しくはdata.pyを除いてみると良い）
-    nb_samples1 = 10 #データ数
-    nb_samples2 = 20
-    X = load_kura_tsom(nb_samples1, nb_samples2) #鞍型データ　ob_dim=3, 真のL=2
+    nb_samples1 = 10 #潜在空間１のデータ数
+    nb_samples2 = 20 #潜在空間２のデータ数
+    X = load_kura_tsom(nb_samples1, nb_samples2)[0] #鞍型データ　ob_dim=3, 真のL=2
     #X = create_rasen(nb_samples) #らせん型データ　ob_dim=3, 真のL=1
     #X = create_2d_sin_curve(nb_samples) #sin型データ　ob_dim=2, 真のL=1
     #X = create_big_kura(nb_samples)
     #X = create_cluster(nb_samples)
     #X = load_data()[0]
-    ukr = TUKR(X, latent_dim1, latent_dim2, sigma, prior='random')
-    ukr.fit(epoch, eta)
+    ukr = TUKR(X, nb_samples1, nb_samples2, latent_dim1, latent_dim2, sigma, prior='random')
+    ukr.fit(epoch, ueta, veta)
     #visualize_real_history(load_data(), ukr.history['z'], ukr.history['error'], save_gif=True, filename="seed20")
-    visualize_history(X, ukr.history['f'], ukr.history['z'], ukr.history['error'], save_gif=False, filename="tmp")
+    visualize_history(X, ukr.history['f'], ukr.history['u'], ukr.history['v'], ukr.history['error'], save_gif=True, filename="tmp")
 
     #----------描画部分が実装されたらコメントアウト外す----------
     #ukr.calc_approximate_f(resolution=10)
