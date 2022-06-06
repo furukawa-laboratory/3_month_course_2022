bc08fb425ddb4384dc49f9b3e532e43fcf5bf2ef
yuki-fukunaga <fukunaga.yuki380@mail.kyutech.jp>
2022-06-06 21:37:35 +0900

    TUTRの描画の色いじってみた

--- Lecture_TUKR/TUKR.py
+++ Lecture_TUKR/TUKR.py
@@ -157,13 +157,13 @@ if __name__ == '__main__':
     from Lecture_TUKR.animal import load_date
 
     #各種パラメータ変えて遊んでみてね．
-    epoch = 200 #学習回数
-    sigma1 = 0.1 #カーネルの幅
-    sigma2 = 0.1
-    eta = 8 #学習率
-    latent_dim = 2 #潜在空間の次元
+    epoch = 100 #学習回数
+    sigma1 = 0.2 #カーネルの幅
+    sigma2 = 0.3
+    eta = 5 #学習率
+    latent_dim = 1 #潜在空間の次元
     alpha = 0.01
-    norm = 10
+    norm = 2
     seed = 4
     np.random.seed(seed)
 
@@ -171,10 +171,11 @@ if __name__ == '__main__':
     xsamples = 20 #データ数
     ysamples = 10
     # X = load_kura_tsom(xsamples, ysamples) #鞍型データ　ob_dim=3, 真のL=2
-    X = load_date()[0][:, :, None]
-    animal_label = load_date(retlabel_animal=True)[1]
-    feature_label = load_date(retlabel_feature=True)[2]
-
+    X, truez, z1, z2 = load_kura_tsom(xsamples, ysamples, retz=True)
+    # X = load_date()[0][:, :, None]
+    # animal_label = load_date(retlabel_animal=True)[1]
+    # feature_label = load_date(retlabel_feature=True)[2]
+    zzz = [truez, z1, z2]
     #X = create_rasen(nb_samples) #らせん型データ　ob_dim=3, 真のL=1
     # X = create_2d_sin_curve(nb_samples) #sin型データ　ob_dim=2, 真のL=1
     ukr = UKR(X, latent_dim, sigma1, sigma2, prior='random')
@@ -184,4 +185,5 @@ if __name__ == '__main__':
     #----------描画部分が実装されたらコメントアウト外す----------
     ukr.calc_approximate_fu(resolution=10)
     # ukr.calc_approximate_fv(resolution=10)
-    visualize_history(X, ukr.history['y'], ukr.history['u'], ukr.history['v'], ukr.history['error'], save_gif=True, filename="/Users/furukawashuushi/Desktop/3ヶ月コースGIF/TUKR動物", label1=animal_label, label2=feature_label)
+    # visualize_history(X, ukr.history['y'], ukr.history['u'], ukr.history['v'], ukr.history['error'], save_gif=False, filename="/Users/furukawashuushi/Desktop/3ヶ月コースGIF/TUKR動物", label1=animal_label, label2=feature_label)
+    visualize_history(X, ukr.history['y'], ukr.history['u'], ukr.history['v'], ukr.history['error'], save_gif=False,filename="/Users/furukawashuushi/Desktop/3ヶ月コースGIF/TUKR", zzz=zzz)
--- Lecture_TUKR/data_scratch.py
+++ Lecture_TUKR/data_scratch.py
@@ -1,22 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 
 def load_kura_tsom(xsamples, ysamples, missing_rate=None,retz=False):
-    z1 = np.random.normal(0, 0.5, xsamples)
-    z2 = np.random.normal(0, 0.5, ysamples)
-
+    # z1 = np.random.normal(0, 0.5, xsamples)
+    # z2 = np.random.normal(0, 0.5, ysamples)
+    z1 = np.linspace(-1, 1, xsamples)
+    z2 = np.linspace(-1, 1, ysamples)
     z1_repeated, z2_repeated = np.meshgrid(z1, z2, indexing = 'ij')
-    x1 = z1_repeated #+ np.random.normal(loc=0.0, scale=0.1, size=(xsamples, ysamples))
-    x2 = z2_repeated #+ np.random.normal(loc=0.0, scale=0.1, size=(xsamples, ysamples))
+    x1 = z1_repeated
+    x2 = z2_repeated
     x3 = x1**2 - x2**2
     #ノイズを加えたい時はここをいじる,locがガウス分布の平均、scaleが分散,size何個ノイズを作るか
     #このノイズを加えることによって三次元空間のデータ点は上下に動く
 
     x = np.concatenate((x1[:, :, np.newaxis],x2[:, :, np.newaxis], x3[:, :, np.newaxis]), axis=2)
-    truez = np.concatenate((x1[:, :, np.newaxis],x2[:, :, np.newaxis], x3[:, :, np.newaxis]), axis=2)
+    x += np.random.normal(loc=0.0, scale=0.1, size=(xsamples, ysamples, 3))
+    truez = np.concatenate((x1[:, :, np.newaxis],x2[:, :, np.newaxis]), axis=2)
+
+
+
     if missing_rate == 0 or missing_rate == None:
         if retz:
-            return x, truez
+            return x, truez, z1, z2
         else:
             return x
 
