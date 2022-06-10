49575b4c1ff56e5eba4fb3bbb43f6ddeb9962645
yuki-fukunaga <fukunaga.yuki380@mail.kyutech.jp>
2022-06-10 16:14:42 +0900

    gitignoreに顔画像のやつ入れたよ〜

---
+++ Lecture_UKR/fukunaga/load.py
@@ -0,0 +1,50 @@
+import numpy as np
+from PIL import Image
+img = np.array(Image.open('/Users/furukawashuushi/Desktop/-5/A_01_-05.jpg'))
+print(img.ndim)
+print(img.shape)
+import os
+
+def load_angle_resized_data():
+    datastore_name = 'datestore/Angle_resized/'
+    dir_list = os.listdir(datastore_name)
+    #file_name = '/-5/A_01_-05.jpg'
+
+    # directory_path = os.path.join(os.path.dirname(__file__), datastore_name)
+    # file_path = os.path.join(directory_path, file_name)
+
+    dir_name = dir_list[0]
+    user_name = '/A_01_'
+    img = []
+    file_list = os.listdir(datastore_name + dir_name)
+    print(dir_name)
+    for file_name in dir_list:
+        if '-' in file_name:
+            if '-5' == file_name:
+                image = np.array(Image.open(datastore_name + file_name + user_name + '-05' + '.jpg'))
+                img.append(image)
+            else:
+                image = np.array(Image.open(datastore_name + file_name + user_name + file_name + '.jpg'))
+                img.append(image)
+
+
+        elif '0' == file_name:
+            image = np.array(Image.open(datastore_name + file_name + user_name + file_name + '.jpg'))
+            img.append(image)
+
+        else:
+            if '5' == file_name:
+                image = np.array(Image.open(datastore_name + file_name + user_name + '+05' + '.jpg'))
+                img.append(image)
+            else:
+                image = np.array(Image.open(datastore_name + file_name + user_name + '+' + file_name + '.jpg'))
+                img.append(image)
+
+    # img = cv2.imread(datastore_name + file_name)
+    #
+    # print(img)
+    # plt.imshow(img)
+    # plt.show()
+    return img
+
+print(load_angle_resized_data())
