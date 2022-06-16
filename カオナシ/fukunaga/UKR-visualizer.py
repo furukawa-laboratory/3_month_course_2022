1deae77d30be276b10dbd5c57c3d7c3149a172a1
yuki-fukunaga <fukunaga.yuki380@mail.kyutech.jp>
2022-06-16 19:20:22 +0900

    Revert "Revert "移動する"" a This reverts commit e250c6deb86d3dd8ab9091fcd7154921489b56c9.

---
+++ カオナシ/fukunaga/UKR-visualizer.py
@@ -0,0 +1,143 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+STEP = 150
+
+
+def visualize_history(X: object, Y_history: object, Z_history: object, error_history: object, save_gif: object = False, filename: object = "tmp") -> object:
+    input_dim, latent_dim = X.shape[1], Z_history[0].shape[1]
+    input_projection_type = '3d' if input_dim > 2 else 'rectilinear'
+
+    fig = plt.figure(figsize=(10, 8))
+    gs = fig.add_gridspec(3, 2)
+    input_ax = fig.add_subplot(gs[0:2, 0], projection=input_projection_type)
+    latent_ax = fig.add_subplot(gs[0:2, 1], aspect='equal')
+    error_ax = fig.add_subplot(gs[2, :])
+    num_epoch = len(Y_history)
+
+    if input_dim == 3 and latent_dim == 2:
+        resolution = int(np.sqrt(Y_history.shape[1]))
+        if Y_history.shape[1] == resolution ** 2:
+            Y_history = np.array(Y_history).reshape((num_epoch, resolution, resolution, input_dim))
+
+    observable_drawer = [None, None, draw_observable_2D,
+                     draw_observable_3D][input_dim]
+    latent_drawer = [None, draw_latent_1D, draw_latent_2D][latent_dim]
+
+    ani = FuncAnimation(
+        fig,
+        update_graph,
+        frames=num_epoch,  # // STEP,
+        repeat=True,
+        interval=50,
+        fargs=(observable_drawer, latent_drawer, X, Y_history, Z_history, error_history, fig,
+               input_ax, latent_ax, error_ax, num_epoch))
+
+    plt.show()
+    if save_gif:
+        ani.save(f"{filename}.mp4", writer='ffmpeg')
+
+# def visualize_history(X: object, Y_history: object, Z_history: object, error_history: object, save_gif=False, object = False, filename: object = "tmp", label = None) -> object:
+#     input_dim, latent_dim = X.shape[1], Z_history[0].shape[1]
+#     input_projection_type = '3d' if input_dim > 2 else 'rectilinear'
+#
+#     fig = plt.figure(figsize=(10, 8))
+#     gs = fig.add_gridspec(3, 2)
+#     # input_ax = fig.add_subplot(gs[0:2, 0], projection=input_projection_type)
+#     latent_ax = fig.add_subplot(gs[0:2, 1], aspect='equal')
+#     error_ax = fig.add_subplot(gs[2, :])
+#     num_epoch = len(Y_history)
+#
+#     if input_dim == 3 and latent_dim == 2:
+#         resolution = int(np.sqrt(Y_history.shape[1]))
+#         if Y_history.shape[1] == resolution ** 2:
+#             Y_history = np.array(Y_history).reshape((num_epoch, resolution, resolution, input_dim))
+#
+#     # observable_drawer = [None, None, draw_observable_2D,
+#     #                     draw_observable_3D][input_dim]
+#     latent_drawer = [None, draw_latent_1D, draw_latent_2D][latent_dim]
+#
+#     ani = FuncAnimation(
+#         fig,
+#         update_graph,
+#         frames=num_epoch,  # // STEP,
+#         repeat=True,
+#         interval=50,
+#         fargs=(latent_drawer, X, Y_history, Z_history, error_history, fig,
+#                latent_ax, error_ax, num_epoch, label))
+#
+#     plt.show()
+#     if save_gif:
+#         ani.save(f"{filename}.mp4", writer='ffmpeg')
+
+
+# def update_graph(epoch, latent_drawer, X, Y_history,
+#                  Z_history, error_history, fig, latent_ax, error_ax, num_epoch, label):
+#     fig.suptitle(f"epoch: {epoch}")
+#     # input_ax.cla()
+#     #  input_ax.view_init(azim=(epoch * 400 / num_epoch), elev=30)
+#     latent_ax.cla()
+#     error_ax.cla()
+#
+#     Y, Z= Y_history[epoch], Z_history[epoch]
+#     colormap = np.array(Z[:, 0])
+#
+#
+#     # observable_drawer(input_ax, X, Y, colormap)
+#     latent_drawer(latent_ax, Z, colormap, label)
+#     draw_error(error_ax, error_history, epoch)
+
+
+def update_graph(epoch,observable_drawer, latent_drawer, X, Y_history,
+                 Z_history, error_history, fig, input_ax, latent_ax, error_ax, num_epoch):
+    fig.suptitle(f"epoch: {epoch}")
+    input_ax.cla()
+    #  input_ax.view_init(azim=(epoch * 400 / num_epoch), elev=30)
+    latent_ax.cla()
+    error_ax.cla()
+
+    Y, Z= Y_history[epoch], Z_history[epoch]
+    colormap = X[:, 0]
+
+    observable_drawer(input_ax, X, Y, colormap)
+    latent_drawer(latent_ax, Z, colormap)
+    draw_error(error_ax, error_history, epoch)
+
+
+def draw_observable_3D(ax, X, Y, colormap):
+    ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=colormap)
+    # ax.set_zlim(-1, 1)
+    if len(Y.shape) == 3:
+        ax.plot_wireframe(Y[:, :, 0], Y[:, :, 1], Y[:, :, 2], color='black')
+        # ax.scatter(Y[:, :, 0], Y[:, :, 1], Y[:, :, 2], color='black')
+    else:
+        ax.plot(Y[:, 0], Y[:, 1], Y[:, 2], color='black')
+# ax.plot(Y[:, 0], Y[:, 1], Y[:, 2], color='black')
+# ax.plot_wireframe(Y[:, :, 0], Y[:, :, 1], Y[:, :, 2], color='black')
+
+
+def draw_observable_2D(ax, X, Y, colormap):
+    ax.scatter(X[:, 0], X[:, 1], c=colormap)
+    ax.plot(Y[:, 0], Y[:, 1], c='black')
+
+
+def draw_latent_2D(ax, Z, colormap):
+    # ax.set_xlim(-1.1, 1.1)
+    # ax.set_ylim(-1.1, 1.1)
+    ax.set_xlim(np.min(Z), np.max(Z))
+    ax.set_ylim(np.min(Z), np.max(Z))
+    ax.scatter(Z[:, 0], Z[:, 1], c=colormap)
+    # for i in range (Z.shape[0]):
+    #      ax.annotate(label[i], xy = (Z[i, 0], Z[i, 1]))
+
+
+def draw_latent_1D(ax, Z, colormap):
+    ax.scatter(Z, np.zeros(Z.shape), c=colormap)
+    # ax.set_ylim(-1, 1)
+    ax.set_ylim(np.min(Z), np.max(Z))
+
+def draw_error(ax, error_history, epoch):
+    ax.set_title("error_function", fontsize=8)
+    ax.plot(error_history, label='誤差関数')
+    ax.scatter(epoch, error_history[epoch], s=55, marker="*")
